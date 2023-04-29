a573c1410a0ad433d48bc092fd862ee1f4e88a83
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2023-04-30 00:20:43 +0330

    calculate the score of the classifier model

--- classification_example.py
+++ classification_example.py
@@ -15,5 +15,19 @@ print(data.target.shape)
 print(data.feature_names)
 
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_train, y_train))
+print(model.score(X_test, y_test))
+
+predictions = model.predict(X_test)
+print(predictions)
+print(model.score(X_test, predictions))
+
+N = len(y_test)
+manual_score = np.sum(predictions == y_test) / N
+print(manual_score)
