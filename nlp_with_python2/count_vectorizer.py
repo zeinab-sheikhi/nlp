281d2a8802baa9645f0db6231b4313e32630c60a
Zeinab Sheikhi <Z.Sheikhi@citydi.com>
2023-05-21 17:37:34 +0330

    analyzer argument in the CountVectorizer object to specify the type of the tokenization

--- nlp_with_python2/count_vectorizer.py
+++ nlp_with_python2/count_vectorizer.py
@@ -2,4 +2,8 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(analyzer="word")
+vectorizer = CountVectorizer(analyzer="char")
+Xtrain = vectorizer.fit_transform()
+
+Xtest = vectorizer.transform()
