ec5c3db92c21c2c7ee9c3c076dc8dd06d5ce7843
Zeinab Sheikhi <Z.Sheikhi@citydi.com>
2023-05-21 17:56:43 +0330

    stopwords in nltk

--- nlp_with_python2/count_vectorizer.py
+++ nlp_with_python2/count_vectorizer.py
@@ -2,8 +2,14 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-vectorizer = CountVectorizer(analyzer="word")
-vectorizer = CountVectorizer(analyzer="char")
-Xtrain = vectorizer.fit_transform()
+# vectorizer = CountVectorizer(analyzer="word")
+# vectorizer = CountVectorizer(analyzer="char")
+# Xtrain = vectorizer.fit_transform()
 
-Xtest = vectorizer.transform()
+# Xtest = vectorizer.transform()
+
+import nltk
+nltk.download("stopwords")
+
+from nltk.corpus import stopwords
+print(stopwords.words('english'))
